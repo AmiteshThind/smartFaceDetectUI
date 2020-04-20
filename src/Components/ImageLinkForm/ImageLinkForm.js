9b3d048f45b78aa7c07f7b5ccc19c58fa1fc54ca
Amitesh Thind <31372481+AmiteshThind@users.noreply.github.com>
2020-04-20 13:49:27 -0400

    Update ImageLinkForm.js

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -4,7 +4,7 @@ import "./ImageLinkForm.css"
 const ImageLinkForm = ({onInputChange,onSubmit})=>{
     return(
         <div>
-            <p className="f3">{'This Magic brain will detect the faces in any image you upload. Try it out.'}</p>
+            <p className="f3">{'This Magic brain will detect the faces of any image you upload. Try it out.'}</p>
             <div className='center'>
                 <div className="center form pa4 br3 shadow-5">
                 <input className='form-control f4 pa2 w-70 center' type='text' onChange={onInputChange} />
